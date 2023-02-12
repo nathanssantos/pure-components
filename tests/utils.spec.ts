8847c100e94ee132db0f3a88f524d49e0e025bb3
Nathan Silva Santos <nathan@w16.com.br>
2023-02-12 16:52:52 -0300

    test: utils

--- tests/utils.spec.ts
+++ tests/utils.spec.ts
@@ -3,8 +3,12 @@ import generateUUID from '../src/utils/generateUUID';
 describe('Utils', () => {
   describe('generateUUID', () => {
     it('Should generate a 16 characters universal unique identifier.', () => {
-      const uuid = generateUUID();
-      expect(uuid).toHaveLength(16);
+      const uuid1 = generateUUID();
+      const uuid2 = generateUUID();
+
+      expect(uuid1).toHaveLength(16);
+      expect(uuid2).toHaveLength(16);
+      expect(uuid1 !== uuid2).toBeTruthy();
     });
   });
 });
