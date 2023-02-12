db2c4ccabca270a5279792edb5ac5d8c7f884c71
Nathan Silva Santos <nathan@w16.com.br>
2023-02-12 18:57:17 -0300

    ensuring full test coverage

---
+++ tests/components/components.spec.ts
@@ -0,0 +1,24 @@
+import Component from '../../src/components/component';
+
+describe('components', () => {
+  describe('component', () => {
+    describe('instance', () => {
+      it('Should create a new component.', () => {
+        const component = new Component();
+
+        expect(component.id).toHaveLength(16);
+        expect(component.type).toBe('div');
+        expect(component.className).toHaveLength(27);
+        expect(component.target).toBeInstanceOf(HTMLElement);
+      });
+    });
+
+    describe('render', () => {
+      it('Should append a component to the target.', () => {
+        new Component({ id: '123' }).render(document.body);
+
+        expect(document.body.querySelector('[data-testid="123"]')).toBeTruthy();
+      });
+    });
+  });
+});
