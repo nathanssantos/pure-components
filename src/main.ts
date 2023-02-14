909f7600db8f46604052f0885fc1da7fcab168a4
Nathan Silva Santos <nathan@w16.com.br>
2023-02-13 21:39:36 -0300

    initial structure

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,25 @@ class App {
 
   renderPreview = () => {
     const drawer = new Drawer();
+    drawer.children.content.children.body.target.innerHTML = `
+      <nav class="menu" style="color: var(--text-color);">
+        <ul style="list-style: none;">
+          <li class="menu__item" style="cursor: pointer;">
+            Item 1
+          </li>
+          <li class="menu__item" style="cursor: pointer;">
+            Item 2
+          </li>
+          <li class="menu__item" style="cursor: pointer;">
+            Item 3
+          </li>
+        </ul>
+      </nav>
+    `;
+
+    drawer.target
+      .querySelectorAll('.menu__item')
+      .forEach((link) => link.addEventListener('click', drawer.close));
     document.body.append(drawer.target);
 
     const btOpenDrawer = new Component({
