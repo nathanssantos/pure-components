92770c0aa788e961d84900ca61399c5d0a53a5ab
Nathan Silva Santos <nathan@w16.com.br>
2023-03-08 00:40:45 -0300

    docs and npm deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     rollupOptions: {
       output: {
