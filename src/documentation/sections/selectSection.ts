76cfeb924e8f96c97e62525d2a2af83ea17461a4
Nathan Silva Santos <nathan@w16.com.br>
2023-04-06 00:31:26 -0300

    Feat: Select

--- src/documentation/sections/selectSection.ts
+++ src/documentation/sections/selectSection.ts
@@ -62,13 +62,9 @@ const selectWithSlots = new Select({
         attributes: { value: '4' },
       }),
     },
-    attributes: {
-      type: 'password',
-      value: 'password_here',
-    },
     events: {
-      input: (_, event: Event) => {
-        console.log((event.target as HTMLInputElement).value);
+      change: (_, event: Event) => {
+        console.log((event.target as HTMLSelectElement).value);
       },
     },
   },
@@ -95,7 +91,7 @@ const componentExample = new Component({
 
 const codeExample = new CodeExample({
   language: 'typescript',
-  content: `import { Input, Component } from '@nathanssantos/pure-components';
+  content: `import { Select, Component } from '@nathanssantos/pure-components';
 
 const simpleSelect = new Select({
   field: {
@@ -129,7 +125,8 @@ const simpleSelect = new Select({
   },
 });
 
-const i18nIcon = '<svg xmlns="http://www.w3.org/2000/svg" fill="none"  width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802" />/svg>';
+const i18nIcon =
+  '<svg xmlns="http://www.w3.org/2000/svg" fill="none"  width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802" />/svg>';
 
 const selectWithSlots = new Select({
   field: {
@@ -155,13 +152,9 @@ const selectWithSlots = new Select({
         attributes: { value: '4' },
       }),
     },
-    attributes: {
-      type: 'password',
-      value: 'password_here',
-    },
     events: {
-      input: (_, event: Event) => {
-        console.log((event.target as HTMLInputElement).value);
+      change: (_, event: Event) => {
+        console.log((event.target as HTMLSelectElement).value);
       },
     },
   },
